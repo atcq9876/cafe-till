c6cf44de0b7be1ea6144a82a0feaa972ce80316f
Andy <ajscottlewis@gmail.com>
2023-04-02 14:23:54 +0100

    Refactor: move table & names validation to private methods

--- order.js
+++ order.js
@@ -1,12 +1,20 @@
 class Order {
   constructor(table, names) {
-    if (typeof table !== 'number') throw new Error('Table must be a number');
-    if (table < 1 || table > 4) throw new Error('There are only four tables in the cafe');
-    if (typeof names !== 'string') throw new Error('Names must be a string');
-    if (names === '') throw new Error('Please enter one or more names');
+    this.#validateTableNumber(table);
+    this.#validateNames(names);
     this.table = table;
     this.names = names;
     this.items = [];
+  }
+
+  #validateTableNumber(table) {
+    if (typeof table !== 'number') throw new Error('Table must be a number');
+    if (table < 1 || table > 4) throw new Error('There are only four tables in the cafe');
+  }
+
+  #validateNames(names) {
+    if (typeof names !== 'string') throw new Error('Names must be a string');
+    if (names === '') throw new Error('Please enter one or more names');
   }
 }
 
