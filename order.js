b5a40e25d83a7eafa1c9d1a2bf368358f83f51fe
Andy <ajscottlewis@gmail.com>
2023-04-02 14:14:55 +0100

    Throw error if table is not type number

--- order.js
+++ order.js
@@ -1,5 +1,6 @@
 class Order {
   constructor(table, names) {
+    if (typeof table !== 'number') throw new Error('Table must be a number');
     this.table = table;
     this.names = names;
     this.items = [];
--- order.test.js
+++ order.test.js
@@ -3,11 +3,25 @@ const Order = require('./order');
 describe('Order', () => {
   it('sets table number, names and empty items', () => {
     const table = 1;
-    const names = "Andy";
+    const names = 'Andy';
     const order = new Order(table, names);
 
     expect(order.table).toEqual(1);
-    expect(order.names).toEqual("Andy");
+    expect(order.names).toEqual('Andy');
     expect(order.items).toEqual([]);
   })
+
+  it('throws error if table is not a number', () => {
+    const table = 'One';
+    const names = 'Andy';
+    
+    expect(() => {
+      const order = new Order(table, names);
+    }).toThrow('Table must be a number');
+  })
 })
+
+
+// expect(() => {
+//   bankAccount.depositFunds('03/01/2023', -10.00);
+// }).toThrow('Only positive amounts can be deposited');
