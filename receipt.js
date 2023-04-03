14d4209d0d3d01308346b86cacac31bd787bbf1f
Andy <ajscottlewis@gmail.com>
2023-04-03 10:00:29 +0100

    Receipt initialises correctly with mocked Order object

--- receipt.js
+++ receipt.js
@@ -1,5 +1,7 @@
 class Receipt {
-
+  constructor(order) {
+    this.order = order;
+  }
 }
 
 module.exports = Receipt;
--- receipt.test.js
+++ receipt.test.js
@@ -1,2 +1,28 @@
 const Receipt = require('./receipt');
+const Order = require('./order');
 
+jest.mock('./order', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      addItem: jest.fn(),
+      removeItem: jest.fn(),
+      getItems: jest.fn().mockReturnValue(['Cafe Latte', 'Cafe Latte', 'Tea']),
+      setTable: jest.fn(),
+      setNames: jest.fn(),
+      getTable: jest.fn().mockReturnValue(2),
+      getNames: jest.fn().mockReturnValue('Andy', 'Anna'),
+    };
+  });
+});
+
+describe('Receipt', () => { 
+  it('initialises correctly if passed an Order object', () => {
+    const mockOrder = new Order();
+    const receipt = new Receipt(mockOrder);
+
+    expect(receipt.order).toEqual(mockOrder);
+    expect(receipt.order.getTable()).toEqual(2);
+    expect(receipt.order.getNames()).toEqual('Andy', 'Anna');
+    expect(receipt.order.getItems()).toEqual(['Cafe Latte', 'Cafe Latte', 'Tea']);
+  })
+})
