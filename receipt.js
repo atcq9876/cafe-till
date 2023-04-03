616b5b2dd23fdef675d5a3e2ab0277a8468b5479
Andy <ajscottlewis@gmail.com>
2023-04-03 10:37:56 +0100

    Refactor: move order validation to private method

--- receipt.js
+++ receipt.js
@@ -2,9 +2,13 @@ const Order = require('./order');
 
 class Receipt {
   constructor(order) {
+    this.#validateOrder(order);
+    this.order = order;
+  }
+
+  #validateOrder(order) {
     if (typeof order !== 'object') throw new Error('Only objects can be passed to Receipt');
     if (typeof order.getTable !== 'function') throw new Error('Only instances of Order can be passed to Receipt');
-    this.order = order;
   }
 }
 
