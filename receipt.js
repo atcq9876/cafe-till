c653bc128b38e8e6b501150ad19c4d3142e3eae2
Andy <ajscottlewis@gmail.com>
2023-04-03 11:03:56 +0100

    Basic cafe info is printed on receipt

--- receipt.js
+++ receipt.js
@@ -6,6 +6,11 @@ class Receipt {
     this.order = order;
   }
 
+  printReceipt() {
+    let receipt = 'The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708';
+    return receipt;
+  }
+
   #validateOrder(order) {
     if (typeof order !== 'object') throw new Error('Only objects can be passed to Receipt');
     if (typeof order.getTable !== 'function') throw new Error('Only instances of Order can be passed to Receipt');
--- receipt.test.js
+++ receipt.test.js
@@ -1,6 +1,7 @@
 const Receipt = require('./receipt');
 const Order = require('./order');
 
+// Mock Order class
 jest.mock('./order', () => {
   return jest.fn().mockImplementation(() => {
     return {
@@ -14,6 +15,37 @@ jest.mock('./order', () => {
     };
   });
 });
+
+// Mock menu object
+const menu = [
+  {
+    "shopName": "The Coffee Connection",
+    "address": "123 Lakeside Way",
+    "phone": "+1 (650) 360-0708",
+    "prices": [
+      {
+        "Cafe Latte": 4.75,
+        "Flat White": 4.75,
+        "Cappucino": 3.85,
+        "Single Espresso": 2.05,
+        "Double Espresso": 3.75,
+        "Americano": 3.75,
+        "Cortado": 4.55,
+        "Tea": 3.65,
+        "Choc Mudcake": 6.40,
+        "Choc Mousse": 8.20,
+        "Affogato": 14.80,
+        "Tiramisu": 11.40,
+        "Blueberry Muffin": 4.05,
+        "Chocolate Chip Muffin": 4.05,
+        "Muffin Of The Day": 4.55
+      }
+    ]
+  }
+];
+
+// Mock the import of the cafeMenu
+jest.mock('./cafeMenu.json', () => menu);
 
 describe('Receipt', () => { 
   it('initialises correctly if passed an Order object', () => {
@@ -36,5 +68,12 @@ describe('Receipt', () => {
     expect(() => {
       new Receipt(new String('string'));
     }).toThrow('Only instances of Order can be passed to Receipt');
+  })
+
+  it('returns the basic cafe info on the receipt', () => {
+    const mockOrder = new Order();
+    const receipt = new Receipt(mockOrder);
+
+    expect(receipt.printReceipt()).toContain('The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708');
   })
 })
