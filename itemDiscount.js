0b94b06c030ea0e4b6b75423a07a039a5afb4ecd
Andy <ajscottlewis@gmail.com>
2023-04-06 11:54:33 +0100

    Refactor: check for item subgroups, not just muffin

--- itemDiscount.js
+++ itemDiscount.js
@@ -20,10 +20,17 @@ class ItemDiscount {
     if (typeof itemName !== 'string') throw new Error('itemName must be a string');
     // Check if item is on the menu or is a muffin (for general muffin discount)
     const menuItems = Object.keys(menu[0].prices[0]);
-    let menuContainsItem = menuItems.includes(itemName) ? true : false;
-    if (menuContainsItem === false && itemName !== 'Muffin') {
+    const menuContainsItem = menuItems.includes(itemName) ? true : false;
+    const menuContainsItemType = this.#validateItemType(itemName);
+    if (menuContainsItem === false && menuContainsItemType === false) {
       throw new Error('That item is not on the menu');
     }
+  }
+
+  // Checks for item subgroups
+  #validateItemType(itemName) {
+    const itemTypes = ['Muffin', 'Coffee', 'Tea', 'Drink', 'Food'];
+    return itemTypes.includes(itemName) ? true : false;
   }
 
   #validateDiscountPercent(discountPercent) {
