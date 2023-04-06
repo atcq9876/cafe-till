1f36c4fbabc829f1de6ba8c26fa905f3c5ef9306
Andy <ajscottlewis@gmail.com>
2023-04-06 10:12:47 +0100

    Create files for totalPriceDiscount

---
+++ totalPriceDiscount.js
@@ -0,0 +1,5 @@
+class TotalPriceDiscount {
+
+}
+
+module.exports = TotalPriceDiscount;
---
+++ totalPriceDiscount.test.js
@@ -0,0 +1,5 @@
+const TotalPriceDiscount = require('./totalPriceDiscount');
+
+describe('TotalPriceDiscount', () => {
+
+})
