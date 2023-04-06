0a5fef89bf6b52c2a177fabe9b7efb58c993b81a
Andy <ajscottlewis@gmail.com>
2023-04-06 11:11:44 +0100

    Add error check to see if itemName is on menu

--- itemDiscount.js
+++ itemDiscount.js
@@ -1,6 +1,13 @@
+const menu = require('./cafeMenu.json');
+
 class ItemDiscount {
   constructor(itemName, discountPercent) {
     if (typeof itemName !== 'string') throw new Error('itemName must be a string');
+    
+    const menuItems = Object.keys(menu[0].prices[0]);
+    let menuContainsItem = menuItems.includes(itemName) ? true : false;
+    if (menuContainsItem === false) throw new Error('That item is not on the menu');
+
     this._itemName = itemName;
     this._discountPercent = discountPercent;
   }
--- itemDiscount.test.js
+++ itemDiscount.test.js
@@ -1,25 +1,56 @@
 const ItemDiscount = require('./itemDiscount');
 
+// Mock menu object
+const menu = [
+  {
+    "shopName": "The Coffee Connection",
+    "address": "123 Lakeside Way",
+    "phone": "+1 (650) 360-0708",
+    "prices": [
+      {
+        "Cafe Latte": 4.75,
+        "Flat White": 4.75,
+        "Cappucino": 3.85,
+        "Single Espresso": 2.05,
+        "Double Espresso": 3.75,
+        "Americano": 3.75,
+        "Cortado": 4.55,
+        "Tea": 3.65,
+        "Choc Mudcake": 6.40,
+        "Choc Mousse": 8.20,
+        "Affogato": 14.80,
+        "Tiramisu": 11.40,
+        "Blueberry Muffin": 4.05,
+        "Chocolate Chip Muffin": 4.05,
+        "Muffin Of The Day": 4.55
+      }
+    ]
+  }
+];
+
+// Mock the import of the cafeMenu
+jest.mock('./cafeMenu.json', () => menu);
+
 describe('ItemDiscount', () => {
   it('initializes correctly', () => {
-    const itemName = 'muffin';
+    const itemName = 'Blueberry Muffin';
     const discountPercent = 10;
     const itemDiscount = new ItemDiscount(itemName, discountPercent);
     
-    expect(itemDiscount._itemName).toEqual('muffin');
+    expect(itemDiscount._itemName).toEqual('Blueberry Muffin');
     expect(itemDiscount._discountPercent).toEqual(10);
   })
 
   it('gets itemName', () => {
-    const itemName = 'Latte';
+    const itemName = 'Cafe Latte';
     const discountPercent = 5;
     const itemDiscount = new ItemDiscount(itemName, discountPercent);
     
-    expect(itemDiscount.getItemName()).toEqual('Latte');
+    expect(itemDiscount.getItemName()).toEqual('Cafe Latte');
   })
 
   it('gets discountPercent', () => {
-    const itemName = 'Latte';
+    const itemName = 'Cafe Latte';
     const discountPercent = 5;
     const itemDiscount = new ItemDiscount(itemName, discountPercent);
     
@@ -33,5 +64,14 @@ describe('ItemDiscount', () => {
     expect(() => {
       new ItemDiscount(itemName, discountPercent);
     }).toThrow('itemName must be a string');
+  })
+
+  test(('throws error if menu does not contain itemName'), () => {
+    const itemName = 'test';
+    const discountPercent = 20;
+    
+    expect(() => {
+      new ItemDiscount(itemName, discountPercent);
+    }).toThrow('That item is not on the menu');
   })
 })
